658db8306fe8dcbad015b1fe6f1447e64ae271cf
Denikaev Emil <edenikaev@mail.ru>
2023-05-19 21:32:53 +0400

    удалил ненужные элементы

---
+++ src/components/lct-main-wrapper.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="lct-main-wrapper">
+    <p>work</p>
+    <!-- <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view /> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "lctMainWrapper",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+// h3 {
+//   margin: 40px 0 0;
+// }
+// ul {
+//   list-style-type: none;
+//   padding: 0;
+// }
+// li {
+//   display: inline-block;
+//   margin: 0 10px;
+// }
+// a {
+//   color: #42b983;
+// }
+
+.lct-main-wrapper {
+  max-width: 900px;
+  margin: 0 auto;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import lctMainWrapper from "../components/lct-main-wrapper";
 
 const routes = [
   {
     path: "/",
     name: "home",
-    component: HomeView,
+    component: lctMainWrapper,
   },
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: lctMainWrapper,
   },
 ];
 
